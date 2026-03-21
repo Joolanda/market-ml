47f35bb404b2ae1481ab3d58994586bde1498870
Joolanda <50619917+Joolanda@users.noreply.github.com>
2026-03-21 12:35:32 +0100

    Add base model and RandomForest implementation

---
+++ src/ml/models/random-forest.py
@@ -0,0 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from .base_model import BaseModel
+
+class RandomForestModel(BaseModel):
+    def __init__(self, **params):
+        self.model = RandomForestClassifier(**params)
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
